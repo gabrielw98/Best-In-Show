33188d0db0367890b333e1b283117449fa41df1a
Gabe Wils <gabrielmwilson98@gmail.com>
2019-12-09 21:05:53 -0800

    S3 Updates

--- DonationTracker/AppDelegate.swift
+++ DonationTracker/AppDelegate.swift
@@ -11,6 +11,8 @@ import Parse
 import UserNotifications
 import AWSS3
 import AWSCore
+import AWSSQS
+import AWSMobileClient
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
@@ -29,20 +31,164 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                 print(keys!["sendGridKey"] as! String)
                 $0.applicationId = (keys!["parseAppId"] as! String)
                 $0.clientKey = (keys!["parseClientKey"] as! String)
-                $0.server = keys!["parseServer"] as! String
+                $0.server = "https://parseapi.back4app.com"
             }
         }
         //7C597q7LCCOeeGumHMdMAeaIAAz9whQmyjmxcL7E
-        let accessKey = "AKIAIR76KKS3FMRBQLNA"
-        let secretKey = "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"
         
-        /*let S3BucketName = "donation-tracker-bucket"
-        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.USEast1,
+        
+        // Don't forget to set up your credentials in AppDelegate.swift and
+        // the imports in your Obj-C bridging-header.
+
+        // Initialize the Amazon Cognito credentials provider
+
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.USWest2,
+           identityPoolId:"us-west-2:52ef1f93-439d-4c8c-a793-54ba31e3b965")
+        let awsConfiguration = AWSServiceConfiguration(region:.USWest2, credentialsProvider:credentialsProvider)
+        AWSServiceManager.default().defaultServiceConfiguration = awsConfiguration
+        
+        
+        
+        /*
+        
+        // Send single message to SQS
+        
+        let createdAt = Date() // convert using a date formatter
+        
+        
+        let queueURL: String = "https://sqs.us-west-2.amazonaws.com/886923046348/VLPostActionsAggRobotQ"
+        
+        // Votes metrics
+        let downVotes = 0
+        let upVotes = 0
+        let reports = 0
+        
+        // Ids
+        let postId = "tk1JmMQdOC"
+        let userObjectId = "c3gwPvVEAm"
+        let vlOrgObjectId = "pO5bAMg5lF"
+        
+        // Date format
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+        let date = dateFormatter.string(from: Date()) + "Z"
+        
+        // Votes data as a dictionary
+        let votesDictionary = ["objectId" : postId, "User" : ["__type": "Pointer", "className": "_User", "objectId" : userObjectId], "VLOrgChannels" : ["__type": "Pointer", "className": "VLOrgChannels", "objectId" : vlOrgObjectId], "createdAt": date, "updatedAt": date, "post_id": postId, "user_id": userObjectId, "vl_org_channels_id": vlOrgObjectId, "upVotes": upVotes, "downVotes": downVotes, "reports": reports] as [String : Any]
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: votesDictionary, options: .prettyPrinted)
+            let convertedString = String(data: jsonData, encoding: String.Encoding.utf8)!
+            print(convertedString)
+            SQS().sendMessage(msg: convertedString, queueURL: queueURL)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        let sentimentsQueueURL: String = "https://sqs.us-west-2.amazonaws.com/886923046348/VLSentimentsQ"
+        
+        let sentimentsDictionary = ["post_id": "r94chztV3K", "channel": "@starbucks", "vl_org_channel_id": "wMtZ3kC9DZ", "user_id": "rjVpsJSrhU", "sentiment": "Rant", "createdAt": "2019-11-18T21:20:25.041Z"]
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: sentimentsDictionary, options: .prettyPrinted)
+            let convertedString = String(data: jsonData, encoding: String.Encoding.utf8)!
+            print(convertedString)
+            SQS().sendMessage(msg: convertedString, queueURL: sentimentsQueueURL)
+        } catch {
+            print(error.localizedDescription)
+        }*/
+        
+        
+        /*let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.USEast1,
                                                                 identityPoolId:"us-east-1:816a224f-f440-4ad0-a342-39b6e2712e2c")
         let configuration2 = AWSServiceConfiguration(region:.USEast1, credentialsProvider:credentialsProvider)
         AWSServiceManager.default().defaultServiceConfiguration = configuration2
+        
+        // Get the queue's URL
+        let getQueueUrlRequest = AWSSQSGetQueueUrlRequest()
+        getQueueUrlRequest!.queueName = queueName
+        sqs.getQueueUrl(getQueueUrlRequest!).continueWith { (task) -> AnyObject! in
+           print("Getting queue URL")
+           if let error = task.error {
+              print(error)
+           }
+           if task.result != nil {
+              if let queueUrl = task.result!.queueUrl {
+                 // Got the queue's URL, try to send the message to the queue
+                 let sendMsgRequest = AWSSQSSendMessageRequest()!
+                 sendMsgRequest.queueUrl = queueUrl
+                 sendMsgRequest.messageBody = "MY TEST MESSAGE!"
+
+                 // Add message attribute if needed
+                 let msgAttribute = AWSSQSMessageAttributeValue()!
+                 msgAttribute.dataType = "String"
+                 msgAttribute.stringValue = "MY ATTRIBUTE VALUE"
+                 sendMsgRequest.messageAttributes = [:]
+                 sendMsgRequest.messageAttributes!["MY_ATTRIBUTE_NAME"] = msgAttribute
+
+                 // Send the message
+                sqs.sendMessage(sendMsgRequest).continueWith { (task) -> AnyObject! in
+                    if let error = task.error {
+                       print(error)
+                    }
+
+                    if task.result != nil {
+                       print("Success! Check the queue on AWS console!")
+                    }
+                    return nil
+                 }
+              } else {
+                 // No URL found, do something?
+              }
+           }
+           return nil
+        }*/
+        
+        
+        //self.uploadToS3()
+        self.getS3Object()
+        
+        /*let S3BucketName = "village-lync"
+        let accessKey = "AKIAIR76KKS3FMRBQLNA"
+        let secretKey = "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"
+        let cp = AWSStaticCredentialsProvider(accessKey: accessKey, secretKey: secretKey)
+        let config = AWSServiceConfiguration(region: AWSRegionType.USWest2, credentialsProvider: credentialsProvider)
+        AWSServiceManager.default().defaultServiceConfiguration = config
         let ext = "jpg"
-        let imageURL = Bundle.main.path(forResource: "test", ofType: ext)!
+        let url = Bundle.main.path(forResource: "test-1", ofType: ext)!
+        
+        let uploadRequest = AWSS3TransferManagerUploadRequest()!
+        
+        uploadRequest.body = URL(fileURLWithPath: url)
+        uploadRequest.key = "image"
+        uploadRequest.bucket = S3BucketName
+        uploadRequest.contentType = "image/jpeg"
+        uploadRequest.acl = .publicRead
+        
+        let transferManager = AWSS3TransferManager.default()
+        transferManager.upload(uploadRequest).continueWith { (task) -> AnyObject? in
+            if let error = task.error {
+                print("Upload failed ❌ (\(error))")
+            }
+            if task.result != nil {
+                let s3URL = NSURL(string: "http://s3.amazonaws.com/\(S3BucketName)/\(uploadRequest.key!)")!
+                print("Uploaded to:\n\(s3URL)")
+            }
+            else {
+                print("Unexpected empty result.")
+            }
+            return nil
+        }*/
+
+        
+        
+        /*
+        let credentialsProvider2 = AWSCognitoCredentialsProvider(regionType:.USEast1,
+                                                                identityPoolId:"us-east-1:816a224f-f440-4ad0-a342-39b6e2712e2c")
+        let configuration2 = AWSServiceConfiguration(region:.USEast1, credentialsProvider:credentialsProvider2)
+        AWSServiceManager.default().defaultServiceConfiguration = configuration2
+        let ext = "jpg"
+        let imageURL = Bundle.main.path(forResource: "test-1", ofType: ext)!
         let localImageURL = URL(fileURLWithPath: imageURL)
         let uploadRequest = AWSS3TransferManagerUploadRequest()
         uploadRequest!.body = localImageURL
@@ -64,8 +210,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                 print("Unexpected empty result.")
             }
             return nil
-        }*/
-        
+        }
+        */
         Parse.initialize(with: configuration)
         //setupNotification()
         if #available(iOS 10.0, *) {
@@ -215,6 +361,77 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
         
     }
+    
+    func getS3Object() {
+        let S3BucketName = "village-lync"
+        
+        let transferManager = AWSS3TransferManager.default()
+        
+        let downloadingFileURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("test-1.jpg")
+        
+        if let downloadRequest = AWSS3TransferManagerDownloadRequest(){
+            downloadRequest.bucket = S3BucketName
+            downloadRequest.key = "325029D2-1127-4CAF-9264-4FDA4CFA0144-8260-0000043E10686E3A.jpg"
+            downloadRequest.downloadingFileURL = downloadingFileURL
+            
+            transferManager.download(downloadRequest).continueWith(executor: AWSExecutor.default(), block: { (task: AWSTask<AnyObject>) -> Any? in
+                if( task.error != nil){
+                    print(task.error!.localizedDescription)
+                    return nil
+                } else {
+                    print("No Error")
+                }
+                
+                print(task.result!)
+                
+                if let data = NSData(contentsOf: downloadingFileURL) {
+                    DispatchQueue.main.async(execute: { () -> Void in
+                        print("Got the data", data)
+                    })
+                }
+            return nil
+            })
+        }
+    }
+    
+    func uploadToS3() {
+        // configure S3
+        let S3BucketName = "village-lync"
+
+        // configure authentication with Cognito
+        let CognitoPoolID = "us-west-2:52ef1f93-439d-4c8c-a793-54ba31e3b965"
+        let Region = AWSRegionType.USWest2
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:Region,
+            identityPoolId:CognitoPoolID)
+        let configuration = AWSServiceConfiguration(region:Region, credentialsProvider:credentialsProvider)
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+        let uploadRequest = AWSS3TransferManagerUploadRequest()!
+        
+        let ext = "jpg"
+        let imageURL = Bundle.main.path(forResource: "test-1", ofType: ext)!
+        let localImageURL = URL(fileURLWithPath: imageURL)
+        
+        uploadRequest.body = localImageURL
+        uploadRequest.key = ProcessInfo.processInfo.globallyUniqueString + "." + ext
+        uploadRequest.bucket = S3BucketName
+        uploadRequest.contentType = "image/" + ext
+        
+        
+        let transferManager = AWSS3TransferManager.default()
+        transferManager.upload(uploadRequest).continueWith { (task) -> AnyObject! in
+            if let error = task.error {
+                print("Upload failed ❌ (\(error))")
+            }
+            if task.result != nil {
+                let s3URL = NSURL(string: "http://s3.amazonaws.com/\(S3BucketName)/\(uploadRequest.key!)")!
+                print("Uploaded to:\n\(s3URL)")
+            }
+            else {
+                print("Unexpected empty result.")
+            }
+            return nil
+        }
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -236,6 +453,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func jsonToString(json: AnyObject){
+        do {
+            let data1 =  try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) // first of all convert json to the data
+            let convertedString = String(data: data1, encoding: String.Encoding.utf8) // the data will be converted to the string
+            print(convertedString) // <-- here is ur string
+
+        } catch let myJSONError {
+            print(myJSONError)
+        }
+
     }
     
     
---
+++ DonationTracker/SQS.swift
@@ -0,0 +1,123 @@
+//
+//  SQS.swift
+//  DonationTracker
+//
+//  Created by Gabe Wilson on 11/7/19.
+//  Copyright © 2019 Gabe Wilson. All rights reserved.
+//
+
+
+import AWSSQS
+import AWSMobileClient
+
+public class SQS {
+    
+    // Send a single message to SQS - No message attributes
+    public func sendMessage(msg: String, queueURL: String) -> () {
+        
+        // Using the AWSMobileClient as the default Identity provider. This uses the Cognito Identity Pool Id specified in the awsconfiguration.json file. If the app user has not authenticated, they'll be issued an unauthenticated identity Id via Cognito Identity Pool
+        let sqs = AWSSQS.default()
+        
+        // Initialize SQS send message request
+        let sendMsgRequest = AWSSQSSendMessageRequest()
+        
+        // Queue URL (not the queue ARN)
+        sendMsgRequest?.queueUrl = queueURL
+        
+        // Message body string (can be JSON as well)
+        sendMsgRequest?.messageBody = msg
+        
+        // (Optional) message attribute. Using it to pass in the user's Identity Id and fake push token
+        var msgAttributes: [String: String] = [:]
+        
+        if let identityId = AWSMobileClient.sharedInstance().identityId {
+            msgAttributes["CognitoIdentityId"] = identityId
+        } else {
+            msgAttributes["CognitoIdentityId"] = "nil"
+        }
+        
+        sendMsgRequest?.messageAttributes = prepareMsgAttributes(keyValuePairs: msgAttributes)
+        
+        sqs.sendMessage(sendMsgRequest!) { (result, err) in
+            if let result = result {
+                print("Successfully sent (1) message. SQS messageId: \(result.messageId)")
+            }
+            if let err = err {
+                print("SQS sendMessage error: \(err)")
+            }
+        }
+    }
+    
+    // Sends a batch of mesasges to SQS
+    public func sendMessageBatch(msgs: [String], queueURL: String) -> () {
+        let sqs = AWSSQS.default()
+        var entries = [AWSSQSSendMessageBatchRequestEntry]()
+        var count = 0
+        
+        // Iterate over messages, add a unique identifier for ea. msg in the batch and then add message to the list of entries
+        for msg in msgs {
+            count += 1
+            let entry = AWSSQSSendMessageBatchRequestEntry()
+            entry?.identifier = String(count) // assign a unique identifier for ea message in batch
+            entry?.messageBody = msg
+            entries.append(entry!)
+        }
+        
+        // Initialize SQS message batch request
+        let sendMessageBatchRequest = AWSSQSSendMessageBatchRequest()
+        sendMessageBatchRequest?.queueUrl = queueURL
+        sendMessageBatchRequest?.entries = entries
+        
+        sqs.sendMessageBatch(sendMessageBatchRequest!) { (result, err) in
+            if let result = result {
+                print("Successfully sent (\(msgs.count)) message(s) via a single batch. SQS result: \(result)")
+            }
+            if let err = err {
+                print("SQS sendMessage error: \(err)")
+            }
+        }
+    }
+    
+    // Add a message attribute to the SQS message (optional)
+    // Each message attribute must have a non-empty name, type, and value
+    func prepareMsgAttributes(keyValuePairs: [String: String]) -> [String: AWSSQSMessageAttributeValue] {
+        var attributes = [String: AWSSQSMessageAttributeValue]()
+    
+        for (name, value) in keyValuePairs {
+            print("Adding KV pair: '\(name)' : '\(value)' to the message attribute.")
+            // Initialize message attribute
+            let msgAttribute = AWSSQSMessageAttributeValue()
+            
+            // Attribute type
+            msgAttribute?.dataType = "String" // Can be String, Number, or Binary
+            
+            // Attribute value
+            msgAttribute?.stringValue = value
+            
+            // Attribute name
+            attributes[name] = msgAttribute
+        }
+        
+        return attributes
+    }
+    
+    // Add a message attribute to the SQS message (optional)
+    // Each message attribute must have a non-empty name, type, and value
+    func msgAttributes(key: String, value: String) -> [String: AWSSQSMessageAttributeValue] {
+        var attributes = [String: AWSSQSMessageAttributeValue]()
+        
+        // Initialize message attribute
+        let msgAttribute = AWSSQSMessageAttributeValue()
+        
+        // Attribute type
+        msgAttribute?.dataType = "String" // Can be String, Number, or Binary
+        
+        // Attribute value
+        msgAttribute?.stringValue = value
+        
+        // Attribute name
+        attributes[key] = msgAttribute
+        
+        return attributes
+    }
+}
